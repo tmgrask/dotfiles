bbc0e1d6f3d0f1b924880a0843d674aebae6efee
tmgrask <t.mackersy@psiphon.ca>
2025-03-31 09:18:23 -0400

    ok?

--- nvim/lua/tasker/init.lua
+++ nvim/lua/tasker/init.lua
@@ -137,7 +137,6 @@ require('lazy').setup({
         config = function()
             local builtin = require('telescope.builtin')
             vim.keymap.set('n', '<C-f>', builtin.find_files, {})
-            vim.keymap.set('n', '<C-g>', builtin.git_files, {})
             vim.keymap.set('n', '<C-s>', builtin.live_grep, {})
         end
     },
